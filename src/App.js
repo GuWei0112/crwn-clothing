1eece17fd49d2b66ecbfc04f6620301965b73012
GuWei0112 <a0983828796@gmai.com>
2019-11-05 23:04:38 +0800

    adding header component logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
-import ShopPage from'./pages/shoppage/shop.component'
+import ShopPage from './pages/shoppage/shop.component'
+import Header from './components/header/header.component'
 import { Route, Switch } from 'react-router-dom'
 function App() {
   return (
     <div >
+      <Header />
       <Switch>
         <Route path='/' component={HomePage} exact />
         <Route path='/shop' component={ShopPage} />
