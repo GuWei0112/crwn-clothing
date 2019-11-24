d8acaf497a8a4de7160770018460a318a3bad1fb
GuWei0112 <a0983828796@gmai.com>
2019-11-24 15:13:40 +0800

    adding the redux persists to the localstorage

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,15 +1,52 @@
 import React from "react";
-
+import { connect } from "react-redux";
+import {
+  clearCartFromItem,
+  addItem,
+  removeItem
+} from "../../redux/cart/cart.action";
 import "./checkout-item.style.scss";
 
-export default ({ cartItem: { name, imageUrl, price, quantity } }) => (
-  <div className="checkout-item">
-    <div className="image-container">
-      <img alt="item" src={imageUrl} />
+const mapDispatchToProps = dispatch => ({
+  clearItem: item => dispatch(clearCartFromItem(item)),
+  addItem: item => dispatch(addItem(item)),
+  removeItem: item => dispatch(removeItem(item))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(({ cartItem, clearItem,addItem,removeItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img alt="item" src={imageUrl} />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">
+        <div
+          className="arrow"
+          onClick={() => {
+            removeItem(cartItem);
+          }}
+        >
+          &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div
+          className="arrow"
+          onClick={() => {
+            addItem(cartItem);
+          }}
+        >
+          &#10095;
+        </div>
+      </span>
+      <span className="price">{price}</span>
+      <div className="remove-button" onClick={() => clearItem(cartItem)}>
+        &#10005;
+      </div>
     </div>
-    <span className="name">{name}</span>
-    <span className="quantity">{quantity}</span>
-    <span className="price">{price}</span>
-    <div className="remove-button">&#10005;</div>
-  </div>
-);
+  );
+});
